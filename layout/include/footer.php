37151126519c7fbe82c4751af3a9171ed6791680
iProgrammer16 <iProgrammer16@gmail.com>
2023-08-20 13:03:44 +0300

    Library project

---
+++ layout/include/footer.php
@@ -0,0 +1,13 @@
+<!-- Start footer -->
+<footer class="text-center">
+        <p>جميع الحقوق محفوظة &copy; 2022 iProgrammer16</p>
+    </footer>
+    <!-- End footer -->
+
+
+    <!-- jQuery -->
+    <script src="js/jquery-3.6.1.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
